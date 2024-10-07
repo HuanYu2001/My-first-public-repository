13533129032dba64f5dce4b435bf759a6a439169
Leo <ddwang1211@gmail.com>
2024-10-07 20:58:58 +0800

    add two integers

---
+++ Add two integers.py
@@ -0,0 +1,8 @@
+#定義函式
+def sum():
+    num1 = int(input("first num:"))
+    num2 = int(input("second num:"))
+    return num1 + num2
+#呼叫函式
+result = sum()
+print("first num add second num = ",result)
